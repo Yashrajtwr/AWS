cf3a14478ea3efe67ec27670c591c766b7ee64b8
yashrajt <yashrajt@10.18.251.70>
2023-11-20 10:47:27 +0530

    commit1

---
+++ src/main/java/com/rest/SpringBootAWS/RestControllerExample.java
@@ -0,0 +1,34 @@
+package com.rest.SpringBootAWS;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@RestController
+//@RequestMapping("/api")
+@Component
+public class RestControllerExample {
+	/*@GetMapping("/aws")
+	public ResponseEntity Display(){
+		return new ResponseEntity("I was landed on AWS cloud",HttpStatus.ACCEPTED);
+		
+	}*/
+	static int i=0;
+	@Scheduled(fixedRate=1000)
+	public void displayTask(){
+		System.out.println("sending message= "+i);
+		i++;
+	}
+		//static int j=2;
+		@Scheduled(cron="*/10 * * * * *")
+		public void displayTask1(){
+			System.out.println("sending cron message=");
+			i++;
+	}
+
+	
+}
---
+++ src/test/java/com/rest/SpringBootAWS/SpringBootAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.SpringBootAWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
